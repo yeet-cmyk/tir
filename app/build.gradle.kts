f3c79810705948c1e451c456557bcd6ba0963fd1
yeet-cmyk <mundher079@gmail.com>
2026-09-16 07:51:06 -0700

    Configure SoundRecovery app

---
+++ app/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.soundrecovery.app"
+    compileSdk = 35
+    defaultConfig {
+        applicationId = "com.soundrecovery.app"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
